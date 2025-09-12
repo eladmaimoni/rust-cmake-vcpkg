28af0409c452bac724b43bf6cb6c0d4a34ce5dae
Elad Maimoni <mr.maimon@gmail.com>
2025-09-12 10:31:16 +0300

    working c++

---
+++ src/ccore/ccore.cpp
@@ -0,0 +1,9 @@
+#include "ccore/ccore.hpp"
+
+namespace by2
+{
+    int32_t by2::ccore_add(int32_t a, int32_t b)
+    {
+        return 0;
+    }
+}
--- src/ccore/ccore/ccore.hpp
+++ src/ccore/ccore/ccore.hpp
@@ -1,1 +1,8 @@
+#pragma once
 
+#include <cstdint>
+
+namespace by2
+{
+    int32_t ccore_add(int32_t a, int32_t b);
+}
