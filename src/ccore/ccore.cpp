d2d6b52ecf6adc5a0b950327c43d53c01ae3de2a
Elad Maimoni <mr.maimon@gmail.com>
2025-09-12 10:38:51 +0300

    using spdlog

--- src/ccore/ccore.cpp
+++ src/ccore/ccore.cpp
@@ -1,9 +1,11 @@
 #include "ccore/ccore.hpp"
+#include <spdlog/spdlog.h>
 
 namespace by2
 {
-    int32_t by2::ccore_add(int32_t a, int32_t b)
+    int32_t ccore_add(int32_t a, int32_t b)
     {
-        return 0;
+        spdlog::info("adding {} + {}, a, b");
+        return a + b;
     }
 }
