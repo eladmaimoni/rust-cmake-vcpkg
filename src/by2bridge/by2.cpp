40d5a7edb9f44afaf17f5c858f4331fcc5379c4d
Elad Maimoni <elmaimon@cisco.com>
2025-09-14 21:24:30 +0300

    added by2 bridge

---
+++ src/by2bridge/by2.cpp
@@ -0,0 +1,9 @@
+
+#include "by2/by2.h"
+#include <ccore/ccore.hpp>
+
+int32_t by2_add(int32_t a, int32_t b)
+{
+    return by2::ccore_add(a, b);
+}
+
---
+++ src/by2bridge/by2/by2.h
@@ -0,0 +1,5 @@
+#include <cstdint>
+
+extern "C" {
+    int32_t by2_add(int32_t a, int32_t b);
+}
