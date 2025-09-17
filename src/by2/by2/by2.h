c03d9f316b7b98125c6a0c015904c3925cedac3c
Elad Maimoni <elmaimon@cisco.com>
2025-09-17 21:14:18 +0300

    Enhance shared library support in CMake configuration
    - Added target_compile_definitions for BY2_SHARED option
    - Updated by2.h to include export/import mechanics for shared libraries

--- src/by2/by2/by2.h
+++ src/by2/by2/by2.h
@@ -1,5 +1,34 @@
+#pragma once
 #include <cstdint>
 
+// Export / import mechanics
+// When building as a shared library (BY2_SHARED option ON), CMake will define:
+//   BY2_BUILD_SHARED privately for the by2 target itself
+//   BY2_USE_SHARED publicly for dependents (consumers)
+// On Windows we map these to __declspec(dllexport/dllimport). On other platforms
+// we use GCC/Clang visibility if available; static builds leave BY2_API empty.
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #if defined(BY2_BUILD_SHARED)
+        #define BY2_API __declspec(dllexport)
+    #elif defined(BY2_USE_SHARED)
+        #define BY2_API __declspec(dllimport)
+    #else
+        #define BY2_API
+    #endif
+#else
+    #if defined(BY2_BUILD_SHARED) && (__GNUC__ >= 4)
+        #define BY2_API __attribute__((visibility("default")))
+    #else
+        #define BY2_API
+    #endif
+#endif
+
+#ifdef __cplusplus
 extern "C" {
-    int32_t by2_add(int32_t a, int32_t b);
-}
+#endif
+
+BY2_API int32_t by2_add(int32_t a, int32_t b);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
