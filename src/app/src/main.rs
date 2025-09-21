7937b66731c8648ea136e2de3b2d5c56dd4fb056
Elad Maimoni <mr.maimon@gmail.com>
2025-09-21 22:00:01 +0300

    dll issues in rust app and flutter app

--- src/app/src/main.rs
+++ src/app/src/main.rs
@@ -1,3 +1,9 @@
+// use bridge::by2_add_safe;
+
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    let y = 10;
+    let result = bridge::by2_add_safe(x, y);
+    println!("The result of adding {} and {} is {}", x, y, result);
 }
